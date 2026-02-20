7563bf1fe8fef25d2796e955314ecc769211604b
Wajahat Ali <wajahatfrontdev@gmail.com>
2026-02-20 23:46:33 +0500

    category

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -4,7 +4,7 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
     projectId: "5601ix16",
     dataset: "production",
-    apiVersion:"v2025-11-05",
+    apiVersion:"2025-02-20",
     useCdn:true,
     token: "skbJyO4j58A6RuS97I0zo8VXZESHxyIuqo3Lyqw6e32dFQLtzZ0pvgORn53rsXtWhFWyEe6ZCwhq0vxbFx5MojIye7xNhcHbwbd4fJgX4HicxKFY0fMT54a4SMkptU8tTaNJmLb2RMnsQX7DJ0C8RYRBKrrwcCEaDsw4JZE9MquzZW8zjCN2",
 })
